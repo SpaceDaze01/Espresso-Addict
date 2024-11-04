112e7a2f32a5b009a8c6bbf8f469b9006651af62
SpaceDaze01 <luunaastar@gmail.com>
2024-11-04 13:32:35 +0100

    update

--- tests/features/step-definitions/Full_screen_mode.js
+++ tests/features/step-definitions/Full_screen_mode.js
@@ -2,7 +2,7 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import { By, until, Key } from 'selenium-webdriver';
 import { expect } from 'chai';
-//KLAR
+
 Given('that the user is on the game page {string}', async function(url){
   await this.driver.get(url);
 });
@@ -17,12 +17,10 @@ When('the user clicks on the {string} icon', async function(fullScreenMode){
 });
 
 Then('the user should be able to play in full screen mode {string}', async function (screenBig) {
-  // Wait until the fullscreen state changes
   await this.driver.wait(async () => {
     return await this.driver.executeScript('return document.fullscreenElement !== null;');
-  }, 5000); // Adjust timeout as necessary
+  }, 5000); 
 
-  // Check if the user is in fullscreen mode
   let isFullscreen = await this.driver.executeScript('return document.fullscreenElement !== null;');
   expect(isFullscreen).to.equal(screenBig === 'true');
  
